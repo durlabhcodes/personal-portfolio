27740b07fa6618a9531fd4b0142d5057f51c5376
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-06-25 00:01:54 +0530

    Working on a basic minimal design for website first

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
-    /*<nav className="flex justify-around p-10 text-[2rem]">
+    <nav className="flex justify-around p-10 text-[2rem] bg-[#2a84d6]">
       <div className="flex w-8/12">
         <div className="flex w-3/5">
           <a className="font-[700]" href="/">
-            D-Code
+            Durlabh Codes
           </a>
         </div>
         <div className="flex w-2/5 text-center">
@@ -16,12 +16,12 @@ export default function Navbar() {
             About
           </a>
           <a className="w-1/3" href="/contact">
-            About
+            Contact
           </a>
         </div>
       </div>
-    </nav>*/
-    <nav className="flex flex-col items-end pr-16 pt-16">
+    </nav>
+    /*<nav className="flex flex-col items-end pr-16 pt-16">
       <div className="font-[700] text-[4rem] text-[#544648] pb-[5rem]">
         Durlabh Codes
       </div>
@@ -29,6 +29,6 @@ export default function Navbar() {
       <div className={styles["nav-items"]}>Work</div>
       <div className={styles["nav-items"]}>About</div>
       <div className={styles["nav-items"]}>Contact</div>
-    </nav>
+    </nav>*/
   );
 }
