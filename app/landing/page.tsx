e4c8640e58fa05827b32f9cd71a5a43a22f28d49
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-06-25 00:01:54 +0530

    WIP : Updated Last Log

---
+++ app/landing/page.tsx
@@ -0,0 +1,16 @@
+import styles from "./Landing.module.css";
+export default function Landing() {
+  return (
+    <div className="w-full">
+      <div className="max-w-2xl mx-auto my-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur
+        at purus nec cursus. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos. Donec ullamcorper fringilla
+        risus, at consequat metus egestas volutpat. Vestibulum nec lectus id
+        quam vulputate rhoncus. Mauris at hendrerit turpis, et venenatis est.
+        Pellentesque ut bibendum urna, pretium sollicitudin turpis. Sed in orci
+        vitae nisi vehicula molestie nec in ex.
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import Navbar from "@/app/navbar/page";
+import Landing from "@/app/landing/page";
 
 export default function Home() {
-  return <Navbar />;
+  // return <Navbar />;
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <Landing />
+    </div>
+  );
 }
