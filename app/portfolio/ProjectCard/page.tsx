4768e8bfbc46f891dab1384786bfb422493dd69d
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-07-09 21:48:19 +0530

    WIP : Trying to fix footer positioning

--- app/portfolio/ProjectCard/page.tsx
+++ app/portfolio/ProjectCard/page.tsx
@@ -6,10 +6,12 @@ interface Project {
 }
 export default function ProjectCard(props: Project) {
   return (
-    <article className="project m-2 bg-green-400">
-      <div className="project-label">{props.name}</div>
-      <div className="project-image">{props.imgUrl}</div>
-      <summary className="project-summary">{props.descThumb}</summary>
+    <article className="project m-2 bg-green-400 flex flex-col items-center">
+      <div className="project-image p-4">
+        <img src="/background.jpg" className="h-[100px]" />
+      </div>
+      <div className="project-label font-bold p-2">{props.name}</div>
+      <div className="project-summary p-2">{props.descThumb}</div>
     </article>
   );
 }
