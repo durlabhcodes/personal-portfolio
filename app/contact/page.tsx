81abcc0b335780dff15fd4f346a47596a5b47fd2
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-07-19 22:40:47 +0530

    Working on project cards

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Contact() {
-  return <section className="contact-details">Contact Page</section>;
+  const socials = [faLinkedin, faGithub, faTwitter, faInstagram];
+  return <section className="contact-details flex-col"></section>;
 }
--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -4,25 +4,25 @@ export default function Navbar() {
   return (
     <nav className="flex justify-around h-[10vh] text-[2rem] /*bg-[#2a84d6]*/ shadow-2xl items-center">
       <div className="flex w-8/12">
-        <div className="flex w-3/5">
+        <div className="flex w-4/5">
           <Link className="font-[700] font-Caprasimo text-5xl" href="/">
             <span className="text-blue-400 ">Durlabh</span> Codes
           </Link>
         </div>
-        <div className="flex w-2/5 text-center items-center">
+        <div className="flex w-1/5 text-center items-center">
           <Link
-            className="w-1/3"
+            className="w-1/2"
             href="https://durlabhsharma.com/"
             target="_blank"
           >
             Blog
           </Link>
-          <Link href="/portfolio" className="w-1/3">
+          <Link href="/portfolio" className="w-1/2">
             Portfolio
           </Link>
-          <Link className="w-1/3" href="/contact">
+          {/*<Link className="w-1/3" href="/contact">
             Contact
-          </Link>
+          </Link>*/}
         </div>
       </div>
     </nav>
