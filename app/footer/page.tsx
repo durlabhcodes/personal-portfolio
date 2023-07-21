1eb41a1067402fa32815285069d0cb2c76fcf375
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-07-21 14:43:06 +0530

    Added Links to Footer Socials

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -9,10 +9,30 @@ import {
 
 export default function Footer() {
   const icons = [faLinkedin, faGithub, faTwitter, faInstagram];
+  const socials = [
+    {
+      icon: faLinkedin,
+      link: "https://www.linkedin.com/in/durlabhsharma/",
+    },
+    {
+      icon: faGithub,
+      link: "https://github.com/durlabhcodes",
+    },
+    {
+      icon: faTwitter,
+      link: "https://twitter.com/durlabhcodes",
+    },
+    {
+      icon: faInstagram,
+      link: "https://www.instagram.com/hannibal.holmes",
+    },
+  ];
   return (
     <footer className="h-[10vh] bg-[#60a5f9] flex justify-center items-center">
-      {icons.map((icon, index) => (
-        <FontAwesomeIcon key={index} icon={icon} className={styles.icon} />
+      {socials.map((social, index) => (
+        <a href={social.link} key={index}>
+          <FontAwesomeIcon icon={social.icon} className={styles.icon} />
+        </a>
       ))}
     </footer>
   );
