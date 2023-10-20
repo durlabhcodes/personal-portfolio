ee58c6a10a694c5276403d21844e6393a14fc26a
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-10-20 22:26:32 +0530

    Making the site responsive. WIP : Responsive navbar

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,20 +1,37 @@
+"use client";
+
 import styles from "./Navbar.module.css";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(false);
+  const toggleNav = () => {
+    setShowNav(!showNav);
+    console.log("Nav Toggled to : " + showNav);
+  };
   return (
-    <div className="navbar flex px-[2rem] lg:px-[20rem] py-[2rem] text-2xl sticky top-0 z-20 bg-white">
-      <span className="cursor-pointer">Durlabh Sharma</span>
-      <div className="hidden lg:flex nav-items-list  grow justify-end">
-        <span className={styles["nav-items"]}>Home</span>
-        <span className={styles["nav-items"]}>Services</span>
-        <span className={styles["nav-items"]}>Blog</span>
-        <span className={styles["nav-items"]}>Contact</span>
+    <div className="navbar flex flex-col lg:flex-row px-[2rem] lg:px-[20rem] py-[2rem] text-2xl sticky top-0 z-20 bg-white">
+      <div className="navbar-wrapper flex grow">
+        <div className="cursor-pointer">Durlabh Sharma</div>
+        <div className="hidden lg:flex nav-items-list  grow justify-end">
+          {/*<span className={styles["nav-items"]}>Home</span>*/}
+          <span className={styles["nav-items"]}>Services</span>
+          <span className={styles["nav-items"]}>Blog</span>
+          <span className={styles["nav-items"]}>Contact</span>
+        </div>
+        <div
+          className="burger-nav flex lg:hidden grow justify-end"
+          onClick={toggleNav}
+        >
+          <FontAwesomeIcon icon={faBars} color="#4d76d1" width="24px" />
+        </div>
       </div>
-      <div className="burger-nav flex lg:hidden grow justify-end">
-        <FontAwesomeIcon icon={faBars} color="#4d76d1" width="24px" />
+      <div className="navbar-small-items lg:hidden flex flex-col items-center">
+        <div>Services</div>
+        <div>Blog</div>
+        <div>Contact</div>
       </div>
     </div>
   );
