6bb3dd3c2a192e19e2153c21da0b20e0b410448d
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-11-21 23:25:51 +0530

    Added Apollo GraphQL Client and connected Hashnode APi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-import { Inter, Martian_Mono, Nunito, Ubuntu } from "next/font/google";
 import { NextFont } from "next/dist/compiled/@next/font";
 import Navbar from "@/app/navbar/page";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Nunito, Ubuntu } from "next/font/google";
+//import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const ubuntu: NextFont = Ubuntu({
   weight: ["700", "400", "300"],
@@ -17,10 +17,10 @@ export const metadata = {
   title: "Durlabh Codes - An attempt was made",
   description: "Durlabh Sharma's attempt at appearing cool in coding community",
 };
-const client = new ApolloClient({
+/*const client = new ApolloClient({
   uri: "https://gql.hashnode.com",
   cache: new InMemoryCache(),
-});
+});*/
 export default function RootLayout({
   children,
 }: {
@@ -29,10 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={nunito.className}>
-        <ApolloProvider client={client}>
-          <Navbar />
-          {children}
-        </ApolloProvider>
+        {/*  <ApolloProvider client={client}>*/}
+        <Navbar />
+        {children}
+        {/*</ApolloProvider>*/}
       </body>
     </html>
   );
