fc3d22fc4560065f687ef1323e478da2555b7c61
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2023-11-22 17:46:24 +0530

    Updated Date Format for Blog Card

--- app/blog/blog-card/BlogCard.tsx
+++ app/blog/blog-card/BlogCard.tsx
@@ -7,10 +7,15 @@ interface BlogCardProps {
   timestamp: string;
 }
 export default function BlogCard(props: BlogCardProps) {
+  const date: Date = new Date(Date.parse(props.timestamp));
+  const dateStr = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`;
+  console.log(props.timestamp + " converted to => " + dateStr);
   return (
     <article className="blog-post border border-solid border-black rounded-lg drop-shadow-2xl cursor-pointer">
       <div
-        className="blog-post-image min-h-[300px] bg-center bg-cover"
+        className="blog-post-image min-h-[300px] bg-center bg-cover rounded-lg"
         style={{ backgroundImage: `url(${props.image})` }}
       ></div>
       <div className="blog-post-details p-4">
@@ -21,7 +26,7 @@ export default function BlogCard(props: BlogCardProps) {
             <span className="inline-block mr-2">
               <FontAwesomeIcon icon={faCalendar} height="16px" />
             </span>
-            {props.timestamp}
+            {dateStr}
           </div>
           <div className="blog-post-read-more text-[#8493a6]">
             Read More &gt;
